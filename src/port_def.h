bec6efc00c0a89f3d230f18d8ff5e20286244a3e
zgtk-guri <11141699+zgtk-guri@users.noreply.github.com>
2018-02-22 09:57:08 +0900

    Add port definitions

---
+++ src/port_def.h
@@ -0,0 +1,24 @@
+#ifndef PORT_DEF_H
+#define PORT_DEF_H
+
+const static int PORT_LED1 = 12;
+const static int PORT_LED2 = 14;
+const static int PORT_SW1 = 26;
+const static int PORT_SW2 = 27;
+const static int PORT_BUZZER = 25;
+const static int PORT_DIPSW1 = 32;
+const static int PORT_DIPSW2 = 33;
+const static int PORT_DOOR_SW = 15;
+const static int PORT_LOCK_DET = 13;
+const static int PORT_SERVO_POW = 17;
+const static int PORT_SERVO_SIG = 16;
+const static int PORT_PN532_POW = 4;
+const static int PORT_PN532_MOSI = 23;
+const static int PORT_PN532_MISO = 19;
+const static int PORT_PN532_SCK = 18;
+const static int PORT_PN532_CS = 5;
+const static int PORT_I2C_SCL = 22;
+const static int PORT_I2C_SDA = 21;
+
+
+#endif
